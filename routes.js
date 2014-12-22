e6181d877b813734eaa8f048360433622ec9da27
Ian Kronquist <iankronquist@teleport.com>
2014-12-21 22:00:54 -0800

    Fix spacing. Remove unused require

--- routes.js
+++ routes.js
@@ -1,10 +1,8 @@
-var nunjucks = require('nunjucks');
-
 module.exports = function(app) {
 
-	app.get('/', function (req, res) {
+  app.get('/', function (req, res) {
 
-		return res.render('play.html', {});
+    return res.render('play.html', {});
 	});
 
 };
